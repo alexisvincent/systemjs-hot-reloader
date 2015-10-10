f031a8b44b7425a32c889bb2f0186cef29d68873
Jiri Spac <capajj@gmail.com>
2015-10-10 16:36:14 +0200

    using System.normalize to get the modules

--- jspm-hot-reloader.js
+++ jspm-hot-reloader.js
@@ -1,51 +1,70 @@
 import Emitter from 'weakee'
 import socketIO from 'socket.io-client'
+
 class JspmHotReloader extends Emitter {
-  constructor(backendUrl) {
+  constructor (backendUrl) {
     super()
     this.socket = socketIO(backendUrl)
     this.socket.on('change', (moduleName) => {
       this.emit('change', moduleName)
       this.hotReload(moduleName)
-    });
-  }
-  hotReload(moduleName) {
-    moduleName = location.origin + '/' + moduleName
-    if (!System._loader.moduleRecords[moduleName]) {
-      return
-    }
-    const module = System._loader.moduleRecords[moduleName]
-    System.delete(moduleName)
-    const toReimport = []
-    function deleteAllImporters(importersToBeDeleted) {
-      importersToBeDeleted.forEach((importer) => {
-        System.delete(importer.name)
-        if (importer.importers.length === 0) {
-          toReimport.push(importer.name)
-        } else {
-          //recourse
-          deleteAllImporters(importer.importers)
-        }
-        console.log('deleted ', importer.name)
-      })
-
-    }
-    if (module.importers.length === 0) {
-      toReimport.push(module.name)
-    } else {
-      deleteAllImporters(module.importers)
-    }
-
-    const promises = toReimport.map((moduleName) => {
-      return System.import(moduleName).then(moduleReloaded => {
-        console.log('reloaded ', moduleName)
-      })
     })
-    Promise.all(promises).then(() => {
-      this.emit('reload', toReimport)
+    this.deleteModule = (moduleToDelete) => {
+      let name = moduleToDelete.name
+      System.delete(name)
+      this.emit('delete', name)
+      console.log('deleted a module ', name)
+    }
+  }
+  getModuleRecord (moduleName) {
+    return System.normalize(moduleName).then(normalizedName => {
+      let aModule = System._loader.moduleRecords[normalizedName]
+      if (!aModule) {
+        return System.normalize(moduleName + '!').then(normalizedName => {
+          let aModule = System._loader.moduleRecords[normalizedName]
+          if (aModule) {
+            return aModule
+          }
+          throw new Error('module was not found in Systemjs moduleRecords')
+        })
+      }
+      return aModule
+    })
+  }
+  hotReload (moduleName) {
+    return this.getModuleRecord(moduleName).then(module => {
+      this.deleteModule(module)
+      const toReimport = []
+      function deleteAllImporters (importersToBeDeleted) {
+        importersToBeDeleted.forEach((importer) => {
+          this.deleteModule(importer.name)
+          if (importer.importers.length === 0) {
+            toReimport.push(importer.name)
+          } else {
+            // recourse
+            deleteAllImporters(importer.importers)
+          }
+        })
+      }
+      if (module.importers.length === 0) {
+        toReimport.push(module.name)
+      } else {
+        deleteAllImporters(module.importers)
+      }
+
+      const promises = toReimport.map((moduleName) => {
+        return System.import(moduleName).then(moduleReloaded => {
+          console.log('reimported ', moduleName)
+        })
+      })
+      return Promise.all(promises).then(() => {
+        this.emit('allReimported', toReimport)
+      })
+    }, (err) => {
+      err
+      // not found any module for this file, not really an error
     })
   }
 }
-let instance
 
 export default JspmHotReloader
