76bb0f01220096a2d2e8d9cc65e0f9de8f481df3
mpfau <matthias.pfau@tutao.de>
2016-01-07 21:14:04 +0100

    fixed a minor bug when . This occurs with jack4it/system-less

--- hot-reloader.js
+++ hot-reloader.js
@@ -167,7 +167,7 @@ class HotReloader extends Emitter {
         })
       }
 
-      if (module.importers.length === 0) {
+      if (typeof module.importers == 'undefined' || module.importers.length === 0) {
         toReimport.push(module.name)
       } else {
         let deleted = deleteAllImporters(module)
