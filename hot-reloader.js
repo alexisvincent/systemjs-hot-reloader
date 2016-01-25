5a809b2b772dd61898750512c662fcdf247db409
peteruithoven <peter@peteruithoven.nl>
2016-01-25 17:30:56 +0100

    Remove unnecessary delete

--- hot-reloader.js
+++ hot-reloader.js
@@ -202,7 +202,6 @@ class HotReloader extends Emitter {
       const deletedModule = this.modulesJustDeleted[moduleName]
       if (deletedModule !== undefined) {
         d('mocking prevInstance module');
-        System.delete(System.normalizeSync(prevInstancePath));
         d('deletedModule.exports: ', deletedModule.exports);
         System.set(prevInstancePath, System.newModule(deletedModule.exports));
       }
