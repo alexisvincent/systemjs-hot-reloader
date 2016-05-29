2506d6668e5de924af4c300b7cb8fde4fc580027
Jiri Spac <capajj@gmail.com>
2016-05-29 21:26:58 +0200

    fixes #92
    removed obsolete way of handling html templates hot reloading

--- hot-reloader.js
+++ hot-reloader.js
@@ -154,26 +154,6 @@ class HotReloader extends Emitter {
     const self = this
     const start = new Date().getTime()
 
-    if (/\.html$/.test(moduleName)) {
-      let moduleImportName = System.normalizeSync(moduleName + '!text')
-      let module = System.loads[moduleImportName]
-      let parentModuleName
-      if (module && module.importers && module.importers.length) {
-        parentModuleName = System.loads[moduleImportName].importers[0].name
-      }
-      if (System.delete(moduleImportName)) {
-        return System.import(moduleImportName)
-          .then(() => {
-            if (parentModuleName) {
-              return this.hotReload(parentModuleName)
-            }
-          })
-      } else {
-        d('failed to delete module')
-        return
-      }
-    }
-
     this.modulesJustDeleted = {}  // TODO use weakmap
     return this.getModuleRecord(moduleName).then(module => {
       this.deleteModule(module, 'origin')
